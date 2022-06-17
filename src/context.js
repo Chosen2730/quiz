a4766ca45cf5ada5f95f1ac93211bc87f53bd73a
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-06-17 21:24:49 +0100

    fixed access code bug

--- src/context.js
+++ src/context.js
@@ -45,23 +45,23 @@ const AppProvider = ({ children }) => {
   const minute = today.getMinutes();
   const future = new Date(year, month, date, hour, minute + 18, second);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const now = new Date().getTime();
-      const endpoint = future.getTime();
-      const duration = endpoint - now;
-      if (duration < 0) {
-        setTime({ min: 0, sec: 0 });
-        showResult(true);
-      } else {
-        const oneMin = 1000 * 60;
-        const min = Math.floor(duration / oneMin);
-        const sec = Math.floor((duration % oneMin) / 1000);
-        setTime({ min, sec });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     const now = new Date().getTime();
+  //     const endpoint = future.getTime();
+  //     const duration = endpoint - now;
+  //     if (duration < 0) {
+  //       setTime({ min: 0, sec: 0 });
+  //       showResult(true);
+  //     } else {
+  //       const oneMin = 1000 * 60;
+  //       const min = Math.floor(duration / oneMin);
+  //       const sec = Math.floor((duration % oneMin) / 1000);
+  //       setTime({ min, sec });
+  //     }
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   const changeAccess = (e) => {
     const value = e.target.value;
