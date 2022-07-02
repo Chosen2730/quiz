93c33161c8421853a582e80cf91ef86599e81364
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-02 10:47:25 +0100

    linked components

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 
-const Options = () => {
+const Options = ({i,option,ans,options,index,q,subject, checkAnswer}) => {
     const [select, setSelect] = useState(false);
     return (
-    <div>options</div>
-  )
+        <p
+            key={i}
+            className='que_p '
+            onClick={() => {
+                setSelect(true)
+                checkAnswer(ans, option, options[i], q, index + 1, subject);
+            }}
+            
+        >
+            {options[i]}. {option}
+        </p>
+    );
 }
 
 export default Options;
